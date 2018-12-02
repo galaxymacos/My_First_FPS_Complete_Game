c96ec302ec41fd12898718aceebf4b5e66a415fc
galaxymacos <galaxymaxx@outlook.com>
2018-12-02 00:33:06 -0500

    Add minimap in boss fight

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+	[SerializeField] private Transform player;
+
+	private void LateUpdate() {
+		transform.position = new Vector3(player.position.x,transform.position.y,player.position.z);
+	}
+}
